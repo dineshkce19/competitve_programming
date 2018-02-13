535d2afa6e08c6b6262596e879f3dae6a466384a
Dinesh Raja <dineshnkce@gmail.com>
2018-02-13 13:12:01 +0530

    Testcase template for contest

---
+++ cpp/numbers/testCaseReader.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// refer -> http://en.cppreference.com/w/cpp/io/ios_base/sync_with_stdio
+	ios::sync_with_stdio(false);
+	int testcase;
+	cin>>testcase;
+	while(testcase--){
+	    //for each testcase perform the respective opreations here
+	}
+	return 0;
+}
